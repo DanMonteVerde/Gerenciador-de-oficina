4ca88df8e0d746a37f0ed93bfc904a33e850ff21
bielzinho77777 <135358002+bielzinho77777@users.noreply.github.com>
2025-10-23 14:24:40 -0300

    .

--- oficina/main/views.py
+++ oficina/main/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def index(request):
     context = {'total_clientes': 5, 'veiculos_manutencao': 10 , 'servicos_concluidos':30, 'orcamentos_pendentes':5 }
     return render(request, 'main/index.html', context)
+def teste(request):
+    return render(request, 'main/teste.html')
