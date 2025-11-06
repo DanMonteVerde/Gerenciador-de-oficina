3a6e7e4c98f97eac523942996a060cadba72906c
DanMonteVerde <tvdan2064@gmail.com>
2025-11-06 16:41:48 -0300

    up

--- oficina/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-def index(request):
-    context = {'total_clientes': 5, 'veiculos_manutencao': 10 , 'servicos_concluidos':30, 'orcamentos_pendentes':5 }
-    return render(request, 'main/index.html', context)
-def teste(request):
-    return render(request, 'main/teste.html')
